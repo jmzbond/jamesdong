110d82ee71677e218a9fa0cfce533d4b54aae81b
James Dong <jdong8@gmail.com>
2014-03-02 21:18:52 -0800

    update domain name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :redirect_to_www
+
+  def redirect_to_www
+    unless /www\.jamesdong\.com/ =~ request.url
+      uri = URI.parse(request.url)
+      uri.host = "www.jamesdong.com"
+      redirect_to uri.to_s
+    end
+  end
+  
 end
