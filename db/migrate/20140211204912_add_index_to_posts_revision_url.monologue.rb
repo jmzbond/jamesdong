09c9fb830becf20320e336cb53252d90853fe7a8
James Dong <jdong8@gmail.com>
2014-07-13 21:29:45 -0700

    removed monologue migration files

--- db/migrate/20140211204912_add_index_to_posts_revision_url.monologue.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding : utf-8 -*-
-# This migration comes from monologue (originally 20120526195147)
-class AddIndexToPostsRevisionUrl < ActiveRecord::Migration
-  def change
-    add_index :monologue_posts_revisions, :url
-  end
-end
--- db/migrate/20140211204916_add_index_to_tag_name.monologue.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding : utf-8 -*-
-# This migration comes from monologue (originally 20120612020023)
-class AddIndexToTagName < ActiveRecord::Migration
-  def change
-    add_index :monologue_tags, :name
-  end
-end
